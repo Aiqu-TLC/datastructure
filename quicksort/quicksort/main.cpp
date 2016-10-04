79c95df7b608b92c8c7828331692c7aa9555932b
Aiqu-TLC <aiqu.fragment@gmail.com>
2016-10-04 22:47:10 +0900

    qsort()

--- quicksort/quicksort/main.cpp
+++ quicksort/quicksort/main.cpp
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#define SWAP(a,b,t) t=a; a=b; b=t;
 
 typedef struct _element
 {
 	int key;
 	char data;
 } element;
-
 typedef element *elemP;
-
 
 void gen_data(int amount);
 void qsort(element a[], int left, int right);
@@ -32,15 +31,18 @@ void gen_data(int amount)
 void qsort(element a[], int left, int right)
 {
 	int pivot, i, j;
-	element temp;
+	element tmp;
 	if (left < right) {
 		i = left; j = right + 1;
 		pivot = a[left].key;
 		do {
 			do i++; while (a[i].key < pivot);
 			do j--; while (a[j].key > pivot);
-
+			if (i < j) SWAP(a[i], a[j], tmp);
 		} while (i < j);
+		SWAP(a[left], a[j], tmp);
+		qsort(a, left, j - 1);
+		qsort(a, j + 1, right);
 	}
 	return;
 }
